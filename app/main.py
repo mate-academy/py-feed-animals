5ca3a0e6c041a3a911e097953f5740aec3442955
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-01-25 15:24:17 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,40 @@
-#  write your code here
+class Animal:
+    def __init__(self, name: str, appetite: int, is_hungry=True):
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self):
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self):
+        print(f"Eating {self.appetite} food points...")
+        if self.is_hungry is True:
+            return self.appetite
+        return 0
+
+
+class Cat(Animal):
+    def __init__(self, name, appetite=3, is_hungry=True):
+        super(Cat, self).__init__(name, appetite, is_hungry)
+
+    @staticmethod
+    def catch_mouse():
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name, appetite=7, is_hungry=True):
+        super(Dog, self).__init__(name, appetite, is_hungry)
+
+    @staticmethod
+    def bring_slippers():
+        print("The slippers delivered!")
+
+
+def feed_animals(animals: list):
+    eaten_points = 0
+    for animal in animals:
+        if animal.is_hungry:
+            eaten_points += animal.appetite
+    return eaten_points
