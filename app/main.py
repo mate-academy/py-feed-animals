a43fcb45571eadbf1743f728d15d6fa6c96540d3
Yurii Kindrat <yurkinkek@gmail.com>
2022-05-22 17:16:18 +0300

    feeding my hungry animals - edit feed_animals func

--- app/main.py
+++ app/main.py
@@ -37,6 +37,6 @@ class Dog(Animal):
 
 
 def feed_animals(list_of_animals: list):
-    return sum([animal.appetite
+    return sum([animal.feed()
                 for animal in list_of_animals
                 if animal.is_hungry])
