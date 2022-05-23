24281a56064a609fc5bb1e0738840bf0859eb493
Yurii Kindrat <yurkinkek@gmail.com>
2022-05-23 15:23:31 +0300

    leave the animals hungry

--- app/main.py
+++ app/main.py
@@ -37,6 +37,6 @@ class Dog(Animal):
 
 
 def feed_animals(list_of_animals: list):
-    return sum([animal.feed()
+    return sum([animal.appetite
                 for animal in list_of_animals
                 if animal.is_hungry])
