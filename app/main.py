2e288ace3960d14793a94855e1dcf4c4cf0472e4
Flashmobber <ufinaid@gmail.com>
2022-05-23 14:27:47 +0300

    All done.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-#  write your code here
+class Animal:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True):
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self):
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self):
+        print(f"Eating {self.appetite} food points...")
+        appetite = 0
+        if self.is_hungry is True:
+            self.is_hungry = False
+            appetite = self.appetite
+            self.appetite = 0
+        return appetite
+
+
+class Cat(Animal):
+    def __init__(self, name: str, is_hungry: bool = True):
+        super().__init__(name, 3, is_hungry)
+
+    @staticmethod
+    def catch_mouse():
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True):
+        super().__init__(name, 7, is_hungry)
+
+    @staticmethod
+    def bring_slippers():
+        print("The slippers delivered!")
+
+
+def feed_animals(animals: list[Animal]):
+    return sum(animal.appetite for animal in animals if animal.is_hungry)
