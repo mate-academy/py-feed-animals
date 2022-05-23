4ad0e06d9f81c7ce39fa1139588a8a16846edce6
Roman <romanschneider9@gmail.com>
2022-05-23 17:09:28 +0300

    inserted keyword arguments for better understanding

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ class Animal:
 
 class Cat(Animal):
     def __init__(self, name, is_hungry=True):
-        super(Cat, self).__init__(name, 3, is_hungry)
+        super(Cat, self).__init__(name=name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse():
@@ -27,7 +27,7 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name, is_hungry=True):
-        super(Dog, self).__init__(name, 7, is_hungry)
+        super(Dog, self).__init__(name=name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers():
