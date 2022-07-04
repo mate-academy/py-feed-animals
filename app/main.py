f36f0af45ed2b90a9d6f8e30ae37b688b5993c20
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-07-04 19:48:55 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,23 +5,33 @@ class Animal:
         self.is_hungry = is_hungry
 
     def print_name(self):
-        return f"Hello, I'm {self.name}"
+        print(f"Hello, I'm {self.name}")
 
     def feed(self):
-        self.is_hungry = False
-        return f"Eating {self.appetite} food points..."
+        if self.is_hungry is True:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        return 0
+
 
 class Cat(Animal):
+    def __init__(self, name: str, is_hungry=True):
+        super(Cat, self).__init__(name, appetite=3, is_hungry=is_hungry)
 
-        def __init__(self, ):
-
-        def catch_mouse(self):
-            return f"The hunt began!"
+    @staticmethod
+    def catch_mouse():
+        print("The hunt began!")
 
 
 class Dog(Animal):
+    def __init__(self, name: str, is_hungry=True):
+        super(Dog, self).__init__(name, appetite=7, is_hungry=is_hungry)
 
-    def __init__(self, ):
+    @staticmethod
+    def bring_slippers():
+        print("The slippers delivered!")
 
-    def bring_slippers:
-        return f"The slippers delivered!"
+
+def feed_animals(animals: list):
+    return sum([animal.feed() for animal in animals])
