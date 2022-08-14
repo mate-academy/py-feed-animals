1222211a3cef07ab70406cb6994e34bdc3ef597a
Diliaver Yusupov <diliaver795@gmail.com>
2022-08-14 23:34:26 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,55 @@
-#  write your code here
+class Animal:
+    list_of_animals = []
+
+    def __init__(self, name, appetite, is_hungry=True):
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+        Animal.list_of_animals.append(self)
+
+    def print_name(self):
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self):
+        if self.is_hungry is True:
+            print(f"Eating {self.appetite} food points...")
+            return self.appetite
+        else:
+            self.is_hungry = False
+            return 0
+
+
+class Cat(Animal):
+    def __init__(self, name, appetite=3, is_hungry=True):
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+        super().__init__(name, appetite, is_hungry)
+
+        Cat.feed(self)
+
+    @staticmethod
+    def catch_mouse():
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name, appetite=7, is_hungry=True):
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+        super().__init__(name, appetite, is_hungry)
+
+    @staticmethod
+    def bring_slippers():
+        print("The slippers delivered!")
+
+
+def feed_animals(list_of_animals):
+    sum_feed_points = 0
+    for i in list_of_animals:
+        if i.is_hungry is True:
+            sum_feed_points += i.appetite
+        if i.is_hungry is True:
+            i.is_hungry = False
+    return sum_feed_points
