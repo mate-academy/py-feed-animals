4c7548881f4797c4c17d43de9003fa3b419e33e6
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-09-25 11:49:48 +0300

    Solution: added type annotations

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 class Animal:
-    def __init__(self, name, appetite, is_hungry=True):
+    def __init__(self, name, appetite, is_hungry=True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-    def print_name(self):
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self):
+    def feed(self) -> int:
         if self.is_hungry is True:
             print(f"Eating {self.appetite} food points...")
             food_points = self.appetite
@@ -17,24 +17,24 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name, appetite=3, is_hungry=True):
+    def __init__(self, name, appetite=3, is_hungry=True) -> None:
         super().__init__(name, appetite, is_hungry)
 
     @staticmethod
-    def catch_mouse():
+    def catch_mouse() -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
-    def __init__(self, name, appetite=7, is_hungry=True):
+    def __init__(self, name, appetite=7, is_hungry=True) -> None:
         super().__init__(name, appetite, is_hungry)
 
     @staticmethod
-    def bring_slippers():
+    def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
-def feed_animals(animals: list):
+def feed_animals(animals: list) -> int:
     result = 0
     for animal in animals:
         result += Animal.feed(animal)
