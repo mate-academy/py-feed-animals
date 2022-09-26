cc4f0cf3c8fbf82a1e5642ca4d6a0d93a65ca41a
Taras Andruschenko <And999110@gmail.com>
2022-09-26 15:43:58 +0300

    Add type annotation

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ class Animal:
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self):
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
