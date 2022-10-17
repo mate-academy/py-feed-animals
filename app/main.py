260651d7448b876acf227c40f03843bd69a2ed35
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-17 17:17:05 +0300

    Class Animal func added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 class Animal:
 
-    def __init__(self, name, appetite, is_hungry):
+    def __init__(self, name: str,
+                       appetite: int, 
+                       is_hungry: bool=True) ->None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
         
+    def print_name(self):
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self):
+        if self.is_hungry is True:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        else:
+            print(f"")
+            return 0
+
+
+
+            
