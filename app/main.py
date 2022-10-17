e13630ca5e202357c69b6159a0f13f02a2372ed5
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-17 15:31:30 +0300

    class Animal

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-#  write your code here
+class Animal:
+
+    def __init__(self, name, appetite, is_hungry):
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+        
