5a3796705984fbaf249f8f9bf9b37f00e5511053
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-18 19:31:40 +0300

    flake8 2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 class Animal:
 
     def __init__(self, name: str,
-                       appetite: int, 
-                       is_hungry: bool=True) -> None:
+                 appetite: int,
+                 is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
-        
+       
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
@@ -28,14 +28,16 @@ class Cat(Animal):
     def catch_mouse(self) -> None:
         print("The hunt began!")
 
+
 class Dog(Animal):
     def __init__(self,
-                 name: str, appetite: int=7,
+                 name: str, appetite: int = 7,
                  is_hungry: bool = True) -> None:
         super().__init__(name, appetite, is_hungry)
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
+        
 
 def feed_animals(list_of_animal: list) -> int:
     return sum([animal.appetite for animal
