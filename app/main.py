9f18d965930602b4e668805fa99fb35f845d1fef
Ihor Ivashov <dm-i@i.ua>
2022-11-02 17:23:05 +0200

    Fixed type annotations

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name=name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
@@ -33,7 +33,7 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name=name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
