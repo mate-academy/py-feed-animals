38411425c69f8fcfbb5cf8c811eac06df5a56a21
Ed Olentsevych <ed.olentsevych@gmail.com>
2022-11-08 15:37:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,51 @@
-#  write your code here
+class Animal:
+
+    def __init__(self, name: str,
+                 appetite: int, is_hungry: bool = True) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.is_hungry is False:
+            return 0
+        print(f"Eating {self.appetite} food points...")
+        self.is_hungry = False
+        return self.appetite
+
+
+class Cat(Animal):
+
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, is_hungry)
+        if is_hungry is False:
+            self.is_hungry = False
+        self.appetite = 3
+
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, is_hungry)
+        if is_hungry is False:
+            self.is_hungry = False
+        self.appetite = 7
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(animals_list: list) -> int:
+    total_food_points = 0
+    for animal in animals_list:
+        one_animal_food_points = animal.feed()
+        total_food_points += one_animal_food_points
+    return total_food_points
