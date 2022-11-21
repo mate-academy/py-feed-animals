be4662f2477b95bf811ba8a77b862808212e1f4a
6aPa6aH <6apa6ah666@gmail.com>
2022-11-21 15:56:54 +0200

    code_fix2

--- app/main.py
+++ app/main.py
@@ -20,20 +20,20 @@ class Animal:
 
 class Cat(Animal):
 
-    def __init__(self, name: str, is_hungry: bool = True) -> None:
-        self.name = name
-        super().__init__(name, appetite=3)
-        self.is_hungry = is_hungry
+    def __init__(self, name: str,
+                 appetite: int = 3,
+                 is_hungry: bool = True) -> None:
+        super().__init__(name, appetite, is_hungry)
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> None:
-        self.name = name
-        super().__init__(name, appetite=7)
-        self.is_hungry = is_hungry
+    def __init__(self, name: str,
+                 appetite: int = 7,
+                 is_hungry: bool = True) -> None:
+        super().__init__(name, appetite, is_hungry)
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
