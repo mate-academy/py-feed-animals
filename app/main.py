cb7563c51b3fba37fae529104529c7f48c7b09f5
Andrii Fedoryshyn <warvar2016wgmail.com>
2022-12-14 15:24:20 +0200

    Solution_for_animals2

--- app/main.py
+++ app/main.py
@@ -18,24 +18,28 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool) -> None:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=True)
         self.is_hungry = is_hungry
-        is_hungry = True
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool) -> None:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7, is_hungry=True)
         self.is_hungry = is_hungry
-        self.is_hungry = True
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
 
 def feed_animals(animals: list) -> int:
-    return sum(animal for animal in animals)
+    return sum(animal.feed() for animal in animals)
+
+
+cat = Cat("Cat", False)
+lion = Animal("Lion", 25, True)
+dog = Dog("Dog")
+print(feed_animals([cat, lion, dog])) == 32
