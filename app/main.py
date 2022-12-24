0526499b56106bbe30f56f11e080d32757a7e167
Vitalii Khmura <vitalikkhmura13@gmail.com>
2022-12-24 19:40:22 +0200

    Solution without "is True"

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
-        if self.is_hungry is True:
+        if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = not self.is_hungry
             return self.appetite
