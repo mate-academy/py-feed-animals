f3daecd8047402344ce9a53e7fb4e0ef63e7d81f
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-01-04 01:16:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,45 @@
-#  write your code here
+from __future__ import annotations
+
+
+class Animal:
+    def __init__(self, name: str,
+                 appetite: int,
+                 is_hungry: bool = True) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> str:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> None | int:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        return 0
+
+
+class Cat(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 3, is_hungry)
+
+    @staticmethod
+    def catch_mouse() -> str:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 7, is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> str:
+        print("The slippers delivered!")
+
+
+def feed_animals(animal_ls: list[Animal]) -> int:
+    sum_hungry = 0
+    for animal in animal_ls:
+        sum_hungry += animal.feed()
+    return sum_hungry
