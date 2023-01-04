78cc047b3fda8eb767d6dfc9cd3c51f770a29e96
Taras Savchyn <savik1992@gmail.com>
2023-01-04 20:32:59 +0200

    rename variable res to food_points

--- app/main.py
+++ app/main.py
@@ -1,11 +1,50 @@
-from app.program_files.animals import Cat, Dog
-from app.program_files.alimal import Animal
+class Animal:
+    def __init__(
+        self, name: str, appetite: int, is_hungry: bool = True
+    ) -> None:
+
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        return 0
 
 
-def feed_animals(animals: list[Animal, Cat, Dog]) -> int:
-    res = 0
+class Cat(Animal):
+    def __init__(
+        self, name: str, is_hungry: bool = True, appetite: int = 3
+    ) -> None:
+
+        super().__init__(name, appetite, is_hungry)
+
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(
+        self, name: str, is_hungry: bool = True, appetite: int = 7
+    ) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(animals: list) -> int:
+    food_points = 0
     for animal in animals:
         if animal.is_hungry:
             animal.feed()
-            res += animal.appetite
-    return res
+            food_points += animal.appetite
+    return food_points
