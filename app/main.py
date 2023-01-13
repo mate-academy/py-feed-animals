b2d40810a60a0c86d6490a7fc20cfebbb1ce6f1e
Sashkoooo <o.denyseko@hotmail.com>
2023-01-13 17:59:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ class Animal:
         self.is_hungry = is_hungry
 
     def print_name(self) -> None:
-        print(f"Hello, I`m {self.name}")
+        print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
         if self.is_hungry:
@@ -21,22 +21,22 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True):
-        super().__init__(name, is_hungry, appetite=3)
-        # self.appetite = 3
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 3, is_hungry)
+
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
 
 
-# lion = Animal("Lion", 25)
-# lion.print_name()  # "Hello, I'm Lion"
-# food_points = lion.feed()  # "Eating 25 food points..."
-# print(food_points)  # 25
-# print(lion.is_hungry)  # False
-# print(lion.feed())  # 0
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 7, is_hungry)
 
-cat = Cat("Cat")
-cat.print_name()  # "Hello, I'm Cat"
-cat.feed()  # "Eating 3 food points"
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
 
-# cat2 = Cat("Cat", False)
-# print(cat2.feed())  # 0
-# cat2.catch_mouse()  # "The hunt began!"
+
+def feed_animals(animals_list: list) -> int:
+    return sum(animal.feed() for animal in animals_list)
