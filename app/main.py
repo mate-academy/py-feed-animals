6bc52ea485e86c413cf032095e6d1d7774711fea
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-07 14:28:30 +0200

    Changed

--- app/main.py
+++ app/main.py
@@ -37,4 +37,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals_list: list) -> int:
-    return sum([animal.feed() for animal in animals_list if animal.is_hungry])
+    return sum([animal.feed() for animal in animals_list])
