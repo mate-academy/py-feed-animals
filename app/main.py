42c5dec611dbe231f274c3d60cdcd39fa7a4759b
Serhii Terletckiy <serhii.terletskiy.ua@gmail.com>
2023-02-07 13:20:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,60 @@
-#  write your code here
+from typing import Union, List
+
+
+class Animal:
+
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> Union[None, int]:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        return 0
+
+
+class Cat(Animal):
+
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True
+    ) -> None:
+        appetite = 3
+        super(Cat, self).__init__(name, appetite, is_hungry)
+
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True
+    ) -> None:
+        appetite = 7
+        super(Dog, self).__init__(name, appetite, is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(lst: List[Animal]) -> int:
+    points = 0
+    for animal in lst:
+        points += animal.feed()
+    return points
