faafe095c5f9f19e46c272fa46f3c71fa2f013e3
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-08 13:16:15 +0200

    2 commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Animal:
-    def __init__(self, name: str, appetite: int,
+    def __init__(self, name: str,
+                 appetite: int,
                  is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
