6e5e7ad086816e0f71a52d731214069ed7bf7da6
David Gluschenko <davidglusch@gmail.com>
2023-02-09 10:41:03 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -21,9 +21,7 @@ class Animal:
 
 class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, appetite=3, is_hungry=True)
-        self.name = name
-        self.is_hungry = is_hungry
+        super().__init__(name=name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -32,9 +30,7 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, appetite=7, is_hungry=True)
-        self.name = name
-        self.is_hungry = is_hungry
+        super().__init__(name=name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
