9e80efc76b6fa51cec6fc0d59ae319d3b9af927e
name <ksyusha0kubay@gmail.com>
2023-02-10 22:03:11 +0200

    'Rechanging_in_line'

--- app/main.py
+++ app/main.py
@@ -21,12 +21,7 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(
-            self,
-            name: str,
-            is_hungry:
-            bool = True
-    ) -> None:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 3, is_hungry)
 
     @staticmethod
