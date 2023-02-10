e2609e455d539b473b8a91e51d5271354c6ead32
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-10 20:24:38 +0200

    use list comprehension

--- app/main.py
+++ app/main.py
@@ -37,7 +37,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals_list: list) -> int:
-    result = 0
-    for animal in animals_list:
-        result += animal.feed()
-    return result
+    return sum([animal.feed() for animal in animals_list])
