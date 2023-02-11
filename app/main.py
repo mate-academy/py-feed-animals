0a91e61230f413226c3e2fc9489f39658452a32b
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-02-11 11:56:24 +0100

    ÃSolution

--- app/main.py
+++ app/main.py
@@ -13,21 +13,16 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
-        if self.is_hungry is True:
+        if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
-            self.is_hungry = False
+            self.is_hungry = not self.is_hungry
             return self.appetite
         return 0
 
 
 class Cat(Animal):
-    def __init__(
-            self,
-            name: str,
-            appetite: int = 3,
-            is_hungry: bool = True
-    ) -> None:
-        super().__init__(name, appetite, is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -35,12 +30,8 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self,
-                 name: str,
-                 appetite: int = 7,
-                 is_hungry: bool = True
-                 ) -> None:
-        super().__init__(name, appetite, is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
@@ -48,6 +39,6 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    return sum([
+    return sum(
         animal.appetite for animal in animals if animal.feed()
-    ])
+    )
