73389d99bc1c115004ccaf6999c507e43be44c20
Viacheslav Shcherbak <misternictof@gmail.com>
2023-02-12 09:21:15 +0200

    Fix errors

--- app/main.py
+++ app/main.py
@@ -48,8 +48,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    result = 0
-    for animal in animals:
-        if animal.is_hungry:
-            result += animal.feed()
-    return result
+    return sum(animal.feed() for animal in animals if animal.is_hungry)
