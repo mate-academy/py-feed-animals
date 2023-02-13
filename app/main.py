cb0cb7c7296f9aa2a5ae845edf4ece5467d82464
Alessa <yvoangel@gmail.com>
2023-02-13 16:12:43 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 class Animal:
-    def __init__(self,
-                 name: str,
-                 appetite: int,
-                 is_hungry: bool = True
-                 ) -> None:
+    def __init__(
+        self, name: str, appetite: int, is_hungry: bool = True
+    ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -20,10 +18,11 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self,
-                 name: str,
-                 is_hungry: bool = True,
-                 ) -> None:
+    def __init__(
+        self,
+        name: str,
+        is_hungry: bool = True,
+    ) -> None:
         super().__init__(name, 3, is_hungry)
 
     @staticmethod
@@ -32,11 +31,11 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self,
-                 name: str,
-                 is_hungry: bool = True,
-                 ) -> None:
-
+    def __init__(
+        self,
+        name: str,
+        is_hungry: bool = True,
+    ) -> None:
         super().__init__(name, 7, is_hungry)
 
     @staticmethod
