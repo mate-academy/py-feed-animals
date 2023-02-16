d5e557f65392f62bf1dd82ec9b4a495fe5695cda
Mykola Savchyn <kola.savchyn@gmail.com>
2023-02-16 15:28:26 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,34 +3,26 @@ class Animal:
     def __init__(self,
                  name: str,
                  appetite: int,
-                 is_hungry: bool = True
-                 ) -> None:
+                 is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
     def print_name(self) -> None:
-        super().__init__()
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
-        super().__init__()
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = not self.is_hungry
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
 
-    def __init__(self,
-                 name: str,
-                 appetite: int = 3,
-                 is_hungry: bool = True
-                 ) -> None:
-        super().__init__(name, appetite, is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -39,12 +31,8 @@ class Cat(Animal):
 
 class Dog(Animal):
 
-    def __init__(self,
-                 name: str,
-                 appetite: int = 7,
-                 is_hungry: bool = True
-                 ) -> None:
-        super().__init__(name, appetite, is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
