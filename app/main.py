0399b4e773fd72762d2eecaab5f907fc9ff9b7a6
kostya-kononenko <kkononenko3@gmail.com>
2023-02-27 14:24:23 +0200

    Solution_1

--- app/main.py
+++ app/main.py
@@ -6,9 +6,8 @@ class Animal:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
-        pass
 
-    def print_name(self) -> str:
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
@@ -25,25 +24,21 @@ class Cat(Animal):
                  name: str,
                  is_hungry: bool = True,
                  appetite: int = 3) -> None:
-        super().__init__(name, appetite)
-        self.is_hungry = is_hungry
+        super().__init__(name, appetite, is_hungry)
 
-    def catch_mouse(self) -> str:
+    @staticmethod
+    def catch_mouse() -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7)
-        self.is_hungry = is_hungry
 
-    def bring_slippers(self) -> str:
+    @staticmethod
+    def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
 def feed_animals(list_of_animals: list[object]) -> int:
-    sum_animal_appetite = 0
-    for animal in list_of_animals:
-        if Animal.feed(animal):
-            sum_animal_appetite += animal.appetite
-    return sum_animal_appetite
+    return sum([Animal.feed(animal) for animal in list_of_animals])
