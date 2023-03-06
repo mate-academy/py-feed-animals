f4e61c058ea26bbadfd174ce331064d7312d32cb
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-03-06 22:17:30 +0200

    Select

--- app/main.py
+++ app/main.py
@@ -43,4 +43,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    return sum(animal.feed() for animal in animals if animal.is_hungry)
+    return sum(animal.feed() for animal in animals)
