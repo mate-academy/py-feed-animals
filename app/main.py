19bf394280894242ca2f2f5a83f0ead899871391
Bohdan Marko <goldenbohdanm@gmail.com>
2023-03-23 15:38:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-#  write your code here
+from app.animal import Animal, Cat, Dog
+
+
+def feed_animals(ls_animals: list[Animal | Cat | Dog]) -> int:
+    feed_points = 0
+    for animal in ls_animals:
+        feed_points += animal.feed()
+
+    return feed_points
