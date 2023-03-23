8010e33c55aef8d7dac8060a08455bc0d69087e6
Bohdan Marko <goldenbohdanm@gmail.com>
2023-03-23 16:57:52 +0200

    Solution v3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.animal import Animal, Cat, Dog
 
 
-def feed_animals(ls_animals: list[Animal | Cat | Dog]) -> int:
-    return sum([animal.feed() for animal in ls_animals])
+def feed_animals(animals: list[Animal | Cat | Dog]) -> int:
+    return sum(animal.feed() for animal in animals)
