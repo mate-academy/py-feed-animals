d4084df5020979285df11c0e32180449fde818ab
Dmytry Bondariev <bondarievmd@gmail.com>
2023-03-26 16:32:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -39,4 +39,4 @@ class Dog(Animal):
 
 
 def feed_animals(list_of_animals: list[Animal]) -> int:
-    return sum(int(animal.feed()) for animal in list_of_animals)
+    return sum(animal.feed() for animal in list_of_animals)
