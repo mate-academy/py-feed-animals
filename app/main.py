364762c1358693d421585f2715bf6ed0a113a9a1
Maksym Filipchuk <maxfiliya@gmail.com>
2023-03-28 19:05:06 +0200

    fixed 2

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ class Animal:
 
     def __init__(
             self, name: str, appetite: int, is_hungry: bool = True
-    ) -> any:
+    ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-    def print_name(self) -> any:
+    def print_name(self) -> None:
         return print(f"Hello, I'm {self.name}")
 
-    def feed(self) -> any:
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -19,22 +19,22 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> any:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
-    def catch_mouse() -> any:
+    def catch_mouse() -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> any:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
-    def bring_slippers() -> any:
+    def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
 def feed_animals(animals: list) -> int:
-    return sum([animal.feed() for animal in animals])
+    return sum(animal.feed() for animal in animals)
