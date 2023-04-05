afc68f9beb2ced6e8185e6bad9f351ec17e0fca2
V Borovets <vborovets0@gmail.com>
2023-04-05 12:08:02 +0300

    Solution feed animals

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
+from __future__ import annotations
+
+
 class Animal:
-    def __init__(self, name, appetite, is_hungry=True):
+    animals = []
+
+    def __init__(self,
+                 name: str,
+                 appetite: int,
+                 is_hungry:
+                 bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
+        Animal.animals.append(self)
 
-    def print_name(self):
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self):
+    def feed(self) -> int:
         if self.appetite > 0 and self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -15,12 +25,30 @@ class Animal:
 
         else:
             self.appetite = 0
-            print(self.appetite)
+            return self.appetite
 
 
-lion = Animal("Lion", 25)
-lion.print_name()
-food_points = lion.feed()  # "Eating 25 food points..."
-print(food_points)
-print(lion.is_hungry)
-print(lion.feed())
+class Cat(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
+
+    def catch_mouse(self) -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
+
+    def bring_slippers(self) -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(list_of_animals: list[Animal]) -> int:
+    food_points = 0
+    for animal in list_of_animals:
+        if animal.is_hungry:
+            food_points += animal.appetite
+            animal.feed()
+
+    return food_points
