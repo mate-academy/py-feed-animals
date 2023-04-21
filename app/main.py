90db72291ea5d95be25226393d4a77745ba63d1f
Petro Havrysh <petro.havrysh@gmail.com>
2023-04-21 14:33:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ class Cat(Animal):
             name: str,
             is_hungry: bool = True
     ) -> None:
-        super().__init__(name, 3, is_hungry)
+        super().__init__(name=name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -38,13 +38,13 @@ class Dog(Animal):
             name: str,
             is_hungry: bool = True
     ) -> None:
-        super().__init__(name, 7, is_hungry)
+        super().__init__(name=name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
-def feed_animals(animals: list) -> int:
+def feed_animals(animals: list[Animal]) -> int:
     total_food_points = sum(animal.feed() for animal in animals)
     return total_food_points
