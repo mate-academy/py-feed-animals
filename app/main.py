2bc12052120ea423b1f5aa71caa00d40fd52f3fd
Roman Fedorchuk <slasherroman@gmail.com>
2023-04-22 17:00:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,60 @@
-#  write your code here
+from __future__ import annotations
+
+
+class Animal:
+    food_point = 0
+
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> None | int:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        return 0
+
+
+class Cat(Animal):
+    def __init__(
+            self,
+            name: str,
+            appetite: int = 3,
+            is_hungry: bool = True
+    ) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    @classmethod
+    def catch_mouse(cls) -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(
+            self,
+            name: str,
+            appetite: int = 7,
+            is_hungry: bool = True
+    ) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    @classmethod
+    def bring_slippers(cls) -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(diff_animals: list[Animal]) -> int:
+    points = 0
+    for animal in diff_animals:
+        points += animal.feed()
+    return points
