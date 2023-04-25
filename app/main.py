b370f73406b1d00a540cbf63bec39b8b884770be
jinjo <vitaly.tyagunov@gmail.com>
2023-04-25 22:23:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-#  write your code here
+from app.animals.animal import Animal
+
+
+def feed_animals(animals: list[Animal]) -> int:
+    sum_of_food_points = 0
+    for animal in animals:
+        sum_of_food_points += animal.feed()
+    return sum_of_food_points
