2d23eacaf97a4c398e36cf643179d31266e1cdee
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-04 19:45:43 +0300

    Fix Py feed animals

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ class Animal:
 
 class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, 3, is_hungry)
+        super().__init__(name=name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -29,7 +29,7 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, 7, is_hungry)
+        super().__init__(name=name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
