0fc1624efb366936e362d4a1577711ebff994e4f
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-06-02 13:23:46 +0300

    Removed arguments from super()__init__

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ class Animal:
 
 class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super(Cat, self).__init__(name, 3, is_hungry)
+        super().__init__(name, 3, is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -30,7 +30,7 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super(Dog, self).__init__(name, 7, is_hungry)
+        super().__init__(name, 7, is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
