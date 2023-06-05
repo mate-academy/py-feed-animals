dacd97108ec1f341f237decc5f92b6b25be16b15
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-06-05 14:52:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ class Animal:
 class Cat(Animal):
 
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super(Cat, self).__init__(name, 3, is_hungry)
+        super().__init__(name, 3, is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -32,7 +32,7 @@ class Cat(Animal):
 class Dog(Animal):
 
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super(Dog, self).__init__(name, 7, is_hungry)
+        super().__init__(name, 7, is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
@@ -40,7 +40,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    total_food_points = 0
-    for animal in animals:
-        total_food_points += animal.feed()
-    return total_food_points
+    return sum(animal.feed() for animal in animals)
