a1869ecc4bb97a48f3f6d5fe2bebe6a4859e6fad
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-05 15:05:29 +0300

    Fixed arguments for Cat, Dog classes

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ class Animal:
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self) -> None:
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -21,10 +21,9 @@ class Animal:
 class Cat(Animal):
     def __init__(self,
                  name: str,
-                 appetite: int = 3,
                  is_hungry: bool = True) -> None:
 
-        super().__init__(name, appetite, is_hungry)
+        super().__init__(name=name, is_hungry=is_hungry, appetite=3)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -34,10 +33,9 @@ class Cat(Animal):
 class Dog(Animal):
     def __init__(self,
                  name: str,
-                 appetite: int = 7,
                  is_hungry: bool = True) -> None:
 
-        super().__init__(name, appetite, is_hungry)
+        super().__init__(name=name, is_hungry=is_hungry, appetite=7)
 
     @staticmethod
     def bring_slippers() -> None:
