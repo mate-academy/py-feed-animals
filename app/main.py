22fae4770510718c2b2d40be81c0c55640018cb9
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-06-15 11:10:08 +0300

    :Solution2

--- app/main.py
+++ app/main.py
@@ -45,8 +45,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    result = 0
-    for animal in animals:
-        if animal.is_hungry is True:
-            result += animal.feed()
-    return result
+    return sum(animal.feed()for animal in animals if animal.is_hungry)
