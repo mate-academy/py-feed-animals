bae601e2a1e81f190b1d8f99fecc097dde0e56dc
Alek Ilchenko <yaichenko87@gmail.com>
2023-06-20 20:45:50 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -47,9 +47,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    return sum(
-        [
-            animal.feed()
-            for animal in animals
-        ]
-    )
+    return sum([animal.feed() for animal in animals])
