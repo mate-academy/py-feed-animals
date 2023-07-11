08f39f5e93448ff2d158d28eed921015059ca072
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-11 14:16:36 +0300

    fixed cat and dog init

--- app/main.py
+++ app/main.py
@@ -20,12 +20,11 @@ class Animal:
 
 class Cat(Animal):
     def __init__(self, name: str,
-                 appetite: int = 3,
                  is_hungry: bool = True
                  ) -> None:
         super().__init__(name=name,
                          is_hungry=is_hungry,
-                         appetite=appetite)
+                         appetite=3)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -34,12 +33,11 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str,
-                 appetite: int = 7,
                  is_hungry: bool = True
                  ) -> None:
         super().__init__(name=name,
                          is_hungry=is_hungry,
-                         appetite=appetite)
+                         appetite=7)
 
     @staticmethod
     def bring_slippers() -> None:
