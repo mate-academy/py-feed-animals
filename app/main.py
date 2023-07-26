afa4e750a6b242e713aae2a9ae5db8fd564ab6c5
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-07-26 13:44:30 +0300

    Annotation fixed

--- app/main.py
+++ app/main.py
@@ -38,5 +38,5 @@ class Dog(Animal):
         print("The slippers delivered!")
 
 
-def feed_animals(animals: list[Animal | Cat | Dog]) -> int:
+def feed_animals(animals: list[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
