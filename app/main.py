95f31a0a27bdad881253a7c9b4466e9c91876b6c
CaCtUs1k <denischernish19012004@gmail.com>
2023-08-11 19:02:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 
 class Animal:
-    def __init__(self,
-                 name: str,
-                 appetite: int,
-                 is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True) -> None:
         self.name = name
         self. appetite = appetite
         self.is_hungry = is_hungry
@@ -40,7 +41,4 @@ class Dog(Animal):
 
 
 def feed_animals(list_hungry: list) -> int:
-    sum_of_appetite = 0
-    for animal in list_hungry:
-        sum_of_appetite += animal.feed()
-    return sum_of_appetite
+    return sum(animal.feed() for animal in list_hungry)
