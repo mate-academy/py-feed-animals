2e864e5e8c76b04a1fbbe8c7365fe22521f6019f
Max Katkalov <makskatkalov@gmail.com>
2023-08-12 08:02:08 +0300

    blacked

--- app/main.py
+++ app/main.py
@@ -1,22 +1,18 @@
 class Animal:
     def __init__(
-            self,
-            name: str,
-            appetite: int,
-            is_hungry: bool = True
+        self,
+        name: str,
+        appetite: int,
+        is_hungry: bool = True
     ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-    def print_name(
-            self
-    ) -> None:
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(
-            self
-    ) -> int:
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -25,16 +21,8 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(
-            self,
-            name: str,
-            is_hungry: bool = True,
-    ) -> None:
-        super().__init__(
-            name,
-            3,
-            is_hungry
-        )
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 3, is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -42,21 +30,13 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(
-            self,
-            name: str,
-            is_hungry: bool = True,
-    ) -> None:
-        super().__init__(
-            name,
-            7,
-            is_hungry
-        )
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 7, is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
-def feed_animals(lst: list[Animal]) -> int:
-    return sum(animal.feed() for animal in lst)
+def feed_animals(list_of_animals: list[Animal]) -> int:
+    return sum(animal.feed() for animal in list_of_animals)
