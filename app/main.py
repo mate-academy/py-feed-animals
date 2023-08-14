83c447361013cb5d254f54194c438e005d459b18
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-08-15 01:24:11 +0300

    feed_animals_v2_dev

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 class Animal:
-
-    def __init__(
-            self,
-            name: str,
-            appetite: int,
-            is_hungry: bool = True) -> None:
+    def __init__(self,
+                 name: str,
+                 appetite: int,
+                 is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -13,19 +11,16 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
-        if self.is_hungry:
-            print(f"Eating {self.appetite} food points...")
-            self.is_hungry = True
-            return self.appetite
-        return 0
+        if not self.is_hungry:
+            return 0
+        print(f"Eating {self.appetite} food points...")
+        self.is_hungry = False
+        return self.appetite
 
 
 class Cat(Animal):
-    def __init__(self,
-                 name: str,
-                 is_hungry: bool = True) -> None:
-        super().__init__(name, 3)
-        self.is_hungry = is_hungry
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super(Cat, self).__init__(name=name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -33,21 +28,13 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self,
-                 name: str,
-                 is_hungry: bool = True) -> None:
-        super().__init__(name, 7)
-        self.is_hungry = is_hungry
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super(Dog, self).__init__(name=name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
-def feed_animals(list_of_animals: list) -> int:
-    sum_of_food = 0
-    for animal in list_of_animals:
-        if animal.is_hungry:
-            animal.feed()
-            sum_of_food += animal.appetite
-    return sum_of_food
+def feed_animals(animals: list["Animal"]) -> int:
+    return sum(animal.feed() for animal in animals)
