223251af2c1310a4eab5564a80bb09e1a56d2a68
Seniv Volodymyr <01156240@pw.edu.pl>
2023-08-24 20:23:06 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -19,9 +19,7 @@ class Animal:
 
 class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(appetite=3)
-        self.name = name
-        self.is_hungry = is_hungry
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
@@ -29,9 +27,7 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(appetite=7)
-        self.name = name
-        self.is_hungry = is_hungry
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
