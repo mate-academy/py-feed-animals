c1d952aa9724a0b918e19110822d27a2904d0d0e
Oleksiy Rozehan <rozeganalexey@gmail.com>
2023-09-15 17:47:23 +0300

    More Pythonic task solution

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
-        if self.is_hungry is False:
+        if not self.is_hungry:
             return 0
         print(f"Eating {self.appetite} food points...")
         self.is_hungry = False
@@ -41,7 +41,5 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    total_food_points = 0
-    for animal in animals:
-        total_food_points += animal.feed()
-    return total_food_points
+    return sum(animal.feed() for animal in animals)
+
