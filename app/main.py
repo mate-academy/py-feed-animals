34440aa631bc5bf29c0fba61ac811d90c939e563
Your_Name Your_Surname <your@email>
2023-10-03 12:58:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,48 @@
-#  write your code here
+class Animal:
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if not self.is_hungry:
+            return 0
+        else:
+            buf = self.appetite
+            self.appetite = 0
+            self.is_hungry = False
+            print(f"Eating {buf} food points...")
+            return buf
+
+
+class Cat(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 3, is_hungry)
+
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 7, is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(list_animals: list["Animal"]) -> int:
+    total = 0
+    for animal in list_animals:
+        total += animal.feed()
+    return total
