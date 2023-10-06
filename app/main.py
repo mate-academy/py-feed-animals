73199436f8b107b02e7c29c633268d0456989e72
Vishnevskyi Dmytro <vishneman@gmail.com>
2023-10-06 14:05:26 +0300

    update main

--- app/main.py
+++ app/main.py
@@ -42,7 +42,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    total_food_points = 0
-    for animal in animals:
-        total_food_points += Animal.feed(animal)
-    return total_food_points
+    return sum(((Animal.feed(animal) for animal in animals)))
