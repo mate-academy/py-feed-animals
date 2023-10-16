c22537fb2d439a620947f6811b58cd5fb664f6ff
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-16 13:42:56 +0300

    refact code

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Animal:
     def __init__(
             self,
@@ -24,7 +27,8 @@ class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
-    def catch_mouse(self) -> None:
+    @staticmethod
+    def catch_mouse() -> None:
         print("The hunt began!")
 
 
@@ -32,9 +36,10 @@ class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
-    def bring_slippers(self) -> None:
+    @staticmethod
+    def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
-def feed_animals(animals: list) -> int:
+def feed_animals(animals: List[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
