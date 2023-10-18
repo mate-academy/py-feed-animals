372fc82327f23c60739274d6339e16f6c1c6acab
Denis Komandyr <sabsdaid@gmail.com>
2023-10-19 02:03:11 +0300

    Feeding animals in 1 line and no useless "else"

--- app/main.py
+++ app/main.py
@@ -17,8 +17,7 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
@@ -38,7 +37,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    food_points_needed = 0
-    for animal in animals:
-        food_points_needed += animal.feed()
-    return food_points_needed
+    return sum(animal.feed() for animal in animals)
