a7c31a9ec43065af96958bfc8ea3e2743aef7c27
Yuliia Dudych <juliadudych@gmail.com>
2023-10-22 18:39:35 +0300

    Define Dog subclass

--- app/main.py
+++ app/main.py
@@ -30,3 +30,14 @@ class Cat(Animal):
     @staticmethod
     def catch_mouse() -> None:
         print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        self.name = name
+        self.is_hungry = is_hungry
+        super().__init__(name, 7, is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
