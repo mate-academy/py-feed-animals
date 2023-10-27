6c2fa13bbf1a961230a01d9062a787f2ba313764
Denysov Viacheslav <wdenisov113@gmail.com>
2023-10-27 13:27:35 +0300

    THE LAST SOLUTIONflake8!

--- app/main.py
+++ app/main.py
@@ -30,10 +30,7 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, is_hungry)
-        self.name = name
-        self.appetite = 7
-        self.is_hungry = True
+        super().__init__(name, 7, is_hungry)
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
