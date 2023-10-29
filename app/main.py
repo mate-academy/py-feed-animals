52b3fb1c3c4adfddd31087c952dcdc0817f14da5
Andrii Malovanyi <82290811+Andrey1104@users.noreply.github.com>
2023-10-29 13:12:37 +0000

    "new fix"

--- app/main.py
+++ app/main.py
@@ -51,8 +51,8 @@ class Dog(Animal):
 
 def feed_animals(animals: list) -> int:
     return sum(
-        [animal.feed()
+        animal.feed()
          for animal in animals
          if isinstance(animal, Animal)
-         and animal.is_hungry is True]
+         and animal.is_hungry is True
     )
