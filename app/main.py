21fb712ad598222a900cb0f6b93cc233d778423e
Oleksandr Tarnavsky <sasha.tarnavsky@gmail.com>
2023-10-31 18:51:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -35,6 +35,7 @@ class Dog(Animal):
 
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super(Dog, self).__init__(name, appetite=7, is_hungry=is_hungry)
+
     @staticmethod
     def bring_slippers() -> str:
         print("The slippers delivered!")
