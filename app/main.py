002055fa222d4b5155ff62ffb1d9d01e6c4edf9e
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-11-09 20:34:41 +0100

    Add missing type annotation

--- app/main.py
+++ app/main.py
@@ -45,5 +45,5 @@ class Dog(Animal):
         print("The slippers delivered!")
 
 
-def feed_animals(animals: list[Animal]):
+def feed_animals(animals: list[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
