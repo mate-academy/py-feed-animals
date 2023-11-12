1309557fef3cdcde1398c40d44b96b58022c0220
ShcherbynaSv <143745512+ShcherbynaSv@users.noreply.github.com>
2023-11-12 21:24:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ class Cat(Animal):
     ) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
-    def catch_mouse(self) -> None:
+    @staticmethod
+    def catch_mouse() -> None:
         print("The hunt began!")
 
 
@@ -43,7 +44,8 @@ class Dog(Animal):
     ) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
-    def bring_slippers(self) -> None:
+    @staticmethod
+    def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
