e664ef34a639442f564cae7df533f93ffb7ab83e
ShcherbynaSv <143745512+ShcherbynaSv@users.noreply.github.com>
2023-11-13 14:19:26 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -30,9 +30,9 @@ class Cat(Animal):
             is_hungry: bool = True
     ) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
-        self.is_hungry = is_hungry
 
-    def catch_mouse(self) -> None:
+    @staticmethod
+    def catch_mouse() -> None:
         print("The hunt began!")
 
 
@@ -42,12 +42,13 @@ class Dog(Animal):
             name: str,
             is_hungry: bool = True
     ) -> None:
-        super().__init__(name, appetite=7)
-        self.is_hungry = is_hungry
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
 
-    def bring_slippers(self) -> None:
+    @staticmethod
+    def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
 def feed_animals(animals: list[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
+
