140fd4ee0d6630b1f7a30d63a4da82d61b10ebb4
Tymur Shmorhun <shmorguntimur@gmail.com>
2023-12-09 01:41:07 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,54 @@
-#  write your code here
+from typing import List
+
+
+class Animal:
+
+    def __init__(
+            self, name: str, appetite: int, is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.is_hungry is True:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        else:
+            return 0
+
+
+class Cat(Animal):
+
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=3)
+        self.name = name
+        self.is_hungry = is_hungry
+
+    def catch_mouse(self) -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=7)
+        self.name = name
+        self.is_hungry = is_hungry
+
+    def bring_slippers(self) -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(ls: List[Animal]) -> int:
+    sum_of_food = 0
+    for animal in ls:
+        if animal.is_hungry is True:
+            animal.feed()
+            sum_of_food += animal.appetite
+
+    return sum_of_food
