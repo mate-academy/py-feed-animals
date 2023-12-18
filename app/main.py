7d4abd250023b8ecd3baadc3a24f8c61e6661c4c
Mykola Kokoiev <veokoknik@gmail.com>
2023-12-18 23:50:33 +0100

    SOL

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Animal:
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self):
+    def feed(self) -> None:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
