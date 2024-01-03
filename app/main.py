1649009e27cd2d41d85e86534cf4a0065b9257cd
Vadim Babii <judviiioff@gmail.com>
2024-01-03 16:55:41 +0200

    Fix no sens condition, add @staticmethod to 36, 27 line

--- app/main.py
+++ app/main.py
@@ -21,23 +21,23 @@ class Animal():
 class Cat(Animal):
 
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name=name, appetite=3, is_hungry=is_hungry)
+        super().__init__(name, 3, is_hungry)
 
-    def catch_mouse(self) -> None:
-        if self.is_hungry:
-            print("The hunt began!")
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
 
 
 class Dog(Animal):
 
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name=name, appetite=7, is_hungry=is_hungry)
+        super().__init__(name, 7, is_hungry)
 
-    def bring_slippers(self) -> None:
-        if self.is_hungry:
-            print("The slippers delivered!")
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    result = sum(animal.feed() for animal in animals if animal.is_hungry)
+    result = sum(animal.feed() for animal in animals)
     return result
