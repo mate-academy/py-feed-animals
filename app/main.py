4889f3a1eb07dc14868d2b1881df3024374de33c
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-01-04 17:06:16 +0200

    fix indentation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 class Animal:
-    def __init__(self, name: str, appetite: int,
-                 is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -17,7 +21,11 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True
+    ) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
@@ -26,7 +34,11 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True
+    ) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
@@ -35,5 +47,5 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    return sum([animal.feed()
-                for animal in animals])
+    return sum(animal.feed()
+               for animal in animals)
