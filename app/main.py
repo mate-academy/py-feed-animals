9ca8ac72a57b8a7bb56720ebf6e76b06cfc032f1
PythonZem <pythonzem@gmail.com>
2024-01-15 12:00:33 +0200

    fix feed method / fix __iinit__ in classes

--- app/main.py
+++ app/main.py
@@ -13,22 +13,21 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
+        food_points = 0
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = not self.is_hungry
-            return self.appetite
-        else:
-            return 0
+            food_points = self.appetite
+        return food_points
 
 
 class Cat(Animal):
     def __init__(
             self,
             name: str,
-            appetite: int = 3,
             is_hungry: bool = True
     ) -> None:
-        super().__init__(name, appetite, is_hungry)
+        super().__init__(name, 3, is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -39,10 +38,9 @@ class Dog(Animal):
     def __init__(
             self,
             name: str,
-            appetite: int = 7,
             is_hungry: bool = True
     ) -> None:
-        super().__init__(name, appetite, is_hungry)
+        super().__init__(name, 7, is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
