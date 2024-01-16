5accc4f93faa5683d295371edc8abdbcc5c1f3c9
Maksym Operchuk <operlast529@gmail.com>
2024-01-16 16:50:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -13,7 +13,9 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
+
         food_point = 0
+
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -30,7 +32,6 @@ class Cat(Animal):
             appetite: int = 3
     ) -> None:
         super().__init__(name, appetite, is_hungry)
-        self.appetite = 3
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
@@ -47,7 +48,9 @@ class Dog(Animal):
 
 
 def feed_animals(animals_list: list[Animal]) -> int:
+
     food_sum = 0
+
     for animal in animals_list:
         food_sum += animal.feed()
     return food_sum
