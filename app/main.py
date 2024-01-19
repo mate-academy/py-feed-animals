a644c287bd277bab7880958e793add33459f3c45
Kyrylo Storozhenko <busterakel@gmail.com>
2024-01-19 16:12:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -23,7 +23,8 @@ class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
-    def catch_mouse(self) -> None:
+    @staticmethod
+    def catch_mouse() -> None:
         print("The hunt began!")
 
 
@@ -36,8 +37,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    food_points = 0
-
-    for animal in animals:
-        food_points += animal.feed()
-    return food_points
+    return sum(animal.feed() for animal in animals)
