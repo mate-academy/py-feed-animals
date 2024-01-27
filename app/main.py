e025c8c7796e96a0fd187a35a39752a41dbb7166
mkuchuman <mkuchuman1@gmail.com>
2024-01-27 18:48:00 +0100

    rewritten in one line

--- app/main.py
+++ app/main.py
@@ -43,7 +43,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: List[Animal]) -> int:
-    result = []
-    for animal in animals:
-        result.append(animal.feed())
-    return sum(result)
+    return sum([animal.feed() for animal in animals])
