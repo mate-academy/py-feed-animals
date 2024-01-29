76597d82f4f6e5f0eda059a43cf34423306d93ef
Vitalii <warcied1@gmail.com>
2024-01-29 15:42:19 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ class Animal:
 
 
 class Cat(Animal):
-
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 3, is_hungry)
 
@@ -30,7 +29,6 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 7, is_hungry)
 
