48858c4d5f2ca6e2dc85aec1bec33ebe038baa75
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-03-02 09:46:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,58 @@
-#  write your code here
+class Animal:
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.appetite > 0:
+            if self.is_hungry:
+                print(f"Eating {self.appetite} food points...")
+                self.is_hungry = False
+                return self.appetite
+        return 0
+
+
+class Cat(Animal):
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True
+    ) -> None:
+        super(Cat, self).__init__(name, appetite=3)
+        self.is_hungry = is_hungry
+        self.name = name
+
+    def catch_mouse(self) -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True
+    ) -> None:
+        super(Dog, self).__init__(name, appetite=7)
+        self.name = name
+        self.is_hungry = is_hungry
+
+    def bring_slippers(self) -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(animals: list[Animal]) -> int:
+    food_points = 0
+    for animal in animals:
+        if animal.is_hungry:
+            food_points += animal.appetite
+            animal.feed()
+    return food_points
