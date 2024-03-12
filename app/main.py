5cc6408112587d8eb4caad591117e5bc0a001092
Maryna Barannik <sober2night@gmail.com>
2024-03-12 11:33:48 +0100

    'Fix'

--- app/main.py
+++ app/main.py
@@ -37,7 +37,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    total_food_points = 0
-    for animal in animals:
-        total_food_points += animal.feed()
-    return total_food_points
+    return sum(animal.feed() for animal in animals)
