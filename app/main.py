731014a05f502f3434c58dfb3fe3873a7a4f2d5f
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-03-21 20:29:11 +0200

    Fixed attributes

--- app/main.py
+++ app/main.py
@@ -21,9 +21,8 @@ class Animal:
 class Cat(Animal):
     def __init__(self,
                  name: str,
-                 is_hungry: bool = True,
-                 appetite: int = 3) -> None:
-        super().__init__(name, appetite, is_hungry)
+                 is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -33,9 +32,8 @@ class Cat(Animal):
 class Dog(Animal):
     def __init__(self,
                  name: str,
-                 is_hungry: bool = True,
-                 appetite: int = 7) -> None:
-        super().__init__(name, appetite, is_hungry)
+                 is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
@@ -43,9 +41,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    all_feed_points = 0
-    for animal in animals:
-        if animal.is_hungry:
-            animal.feed()
-            all_feed_points += animal.appetite
-    return all_feed_points
+    return sum([animal.feed() for animal in animals if animal.is_hungry])
