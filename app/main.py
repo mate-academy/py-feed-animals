f7041f0be6e0712ff4829fa57b18bd595c43bde0
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-03-25 07:45:00 +0000

    we always need to have some kind of alternative in life :D

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 class Animal:
-    def __init__(
-            self,
-            name: str,
-            appetite: int,
-            is_hungry: bool = True
-    ) -> None:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
@@ -17,8 +15,7 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
@@ -46,3 +43,19 @@ def feed_animals(animals: list) -> int:
             animal.feed()
             result += animal.appetite
     return result
+
+
+cat = Cat("Cat", False)
+lion = Animal("Lion", 25, True)
+dog = Dog("Dog")
+feed_animals([cat, lion, dog])
+
+
+"""
+def feed_animals(animals: list) -> int:
+    total_food_points = 0
+    for animal in animals:
+        food_points = animal.feed()
+        total_food_points += food_points
+    return total_food_points
+"""
