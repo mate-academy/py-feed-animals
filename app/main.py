96c2147b5cc7f06e37ef89b7e49e92f4221d1cd4
Vladyslav Prymas <prymas.vladyslav@gmail.com>
2024-03-27 16:06:29 +0200

    Add method sum()

--- app/main.py
+++ app/main.py
@@ -18,8 +18,7 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
@@ -41,7 +40,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    food_amount = 0
-    for animal in animals:
-        food_amount += animal.feed()
-    return food_amount
+    return sum(animal.feed() for animal in animals)
