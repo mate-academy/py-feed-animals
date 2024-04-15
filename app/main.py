2b6ea5037417079495d7c954af5aff221b9b91f6
VR <vladrymarchuk@gmail.com>
2024-04-15 16:30:02 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -12,12 +12,11 @@ class Animal:
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self) -> int | None:
+    def feed(self) -> int:
         if self.is_hungry:
             self.is_hungry = False
             food_points = self.appetite
             print(f"Eating {self.appetite} food points...")
-            self.appetite = 0
             return food_points
         return 0
 
@@ -26,9 +25,8 @@ class Cat(Animal):
     def __init__(
             self,
             name: str,
-            appetite: int = 3,
             is_hungry: bool = True) -> None:
-        super().__init__(name, appetite, is_hungry)
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -39,9 +37,8 @@ class Dog(Animal):
     def __init__(
             self,
             name: str,
-            appetite: int = 7,
             is_hungry: bool = True) -> None:
-        super().__init__(name, appetite, is_hungry)
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
