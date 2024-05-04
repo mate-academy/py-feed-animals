a1bb9b89adf26b23a402c03ada83d61bca0963f1
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-05-04 22:03:18 +0200

    use list comp and delete else

--- app/main.py
+++ app/main.py
@@ -16,8 +16,7 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
@@ -37,7 +36,5 @@ class Dog(Animal):
 
 
 def feed_animals(animals: List[Animal]) -> int:
-    total_food_points = 0
-    for animal in animals:
-        total_food_points += animal.feed()
+    total_food_points = sum([animal.feed() for animal in animals])
     return total_food_points
