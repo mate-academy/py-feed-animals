1dff73a61ab25f725fba769adb14ba30ea205db8
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-05-27 11:12:24 +0300

    revert changes

--- app/main.py
+++ app/main.py
@@ -21,8 +21,7 @@ class Animal:
 class Cat(Animal):
 
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, is_hungry)
-        self.appetite = 3
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -32,8 +31,7 @@ class Cat(Animal):
 class Dog(Animal):
 
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, is_hungry)
-        self.appetite = 7
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
