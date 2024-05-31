00a81130cc038f29452bdf1b0e320842eb248ff1
Vlad <vlad@example.com>
2024-05-31 23:03:47 +0300

    fixed code

--- app/main.py
+++ app/main.py
@@ -40,4 +40,4 @@ class Dog(Animal):
 
 
 def feed_animals(list_animals: list[Animal]) -> int:
-    return sum([animal.feed() for animal in list_animals])
+    return sum(animal.feed() for animal in list_animals)
