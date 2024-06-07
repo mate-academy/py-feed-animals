09458ddb5531662e5fc14952559c799aba161417
Timur <79165970+Timur5050@users.noreply.github.com>
2024-06-07 12:44:38 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -39,4 +39,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals_list: list[Animal]) -> int:
-    return sum[animal.feed() for animal in animals_list]
+    return sum([animal.feed() for animal in animals_list])
