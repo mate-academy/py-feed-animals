f54baf8e33e5e798993e922e653066833831424c
Alex <54539kalashnikov@gmail.com>
2024-06-20 10:06:25 +0300

    One line and dell else

--- app/main.py
+++ app/main.py
@@ -17,8 +17,7 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
@@ -52,7 +51,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    total_food_points = 0
-    for animal in animals:
-        total_food_points += animal.feed()
-    return total_food_points
+    return sum([animal.feed() for animal in animals])
