00d4049c57ceb6854856157d5cb923a0aeff6608
Halyna Rohatska <rohatskahr@gmail.com>
2024-06-21 15:49:30 +0300

    implement Cat and Dog classes and "feed animals" function

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
+from __future__ import annotations
 
 
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-    def print_name(self) ->None:
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
@@ -15,3 +21,28 @@ class Animal:
             self.is_hungry = not self.is_hungry
             return self.appetite
         return 0
+
+
+class Cat(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 3, is_hungry)
+
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 7, is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(animals: list[Animal]) -> int:
+    total_points_of_food = 0
+    for animal in animals:
+        total_points_of_food += animal.feed()
+    return total_points_of_food
