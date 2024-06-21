5494110ab6ec0675fb0bb83b2d3a45e084092784
Halyna Rohatska <rohatskahr@gmail.com>
2024-06-21 14:46:47 +0300

    implement class Animal

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-#  write your code here
+
+
+class Animal:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) ->None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.is_hungry is True:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = not self.is_hungry
+            return self.appetite
+        return 0
