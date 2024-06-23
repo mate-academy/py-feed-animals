de8e2e01d34953a859f0f6e50033d81e575d7773
andreykolomiec <akolomiec982@gmail.com>
2024-06-23 14:25:34 +0300

    annotation for function argument 'is_hungry'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry=True) -> None:
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -16,7 +21,7 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry=True) -> None:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
@@ -24,7 +29,7 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry=True) -> None:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     def bring_slippers(self) -> None:
