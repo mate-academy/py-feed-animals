8c25b5297b1233cd97da0497c4845ea930044d69
r0manvynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-06-28 16:58:22 +0300

    Update app/main.py
    Co-authored-by: Vasyl Hnatiuk <104974881+vasylhnatiuk@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -36,5 +36,5 @@ class Dog(Animal):
         print("The slippers delivered!")
 
 
-def feed_animals(list_of_animals: list[Animal]) -> int:
+def feed_animals(animals: list[Animal]) -> int:
     return sum(animal.feed() for animal in list_of_animals)
