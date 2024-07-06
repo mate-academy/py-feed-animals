9e653b66566a353ba4196073f2613dcb750e242d
Vladyslav Yurchenko <vladyu89@gmail.com>
2024-07-06 21:27:05 +0200

    Fixed solution for py-feed-animals

--- app/main.py
+++ app/main.py
@@ -47,9 +47,5 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    total_food = 0
-    for animal in animals:
-        if isinstance(animal, Animal) and animal.is_hungry:
-            animal.feed()
-            total_food += animal.appetite
-    return total_food
+    return sum(animal.feed() for animal in animals
+               if isinstance(animal, Animal) and animal.is_hungry)
