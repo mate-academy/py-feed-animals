59e2ed22a264d8694b895a654380f6bcda8dfe5f
pavel_t <{89582095}+{PavelTym}@users.noreply.github.com>
2024-07-24 15:50:10 +0300

    using list comprehension for feed_animals func

--- app/main.py
+++ app/main.py
@@ -39,8 +39,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    total_food_points = 0
-    for animal in animals:
-        if animal.feed():
-            total_food_points += animal.appetite
-    return total_food_points
+    return sum([animal.feed() for animal in animals])
