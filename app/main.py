7366d0e6f507ebfcfa83f6c37bc8b093df8a2523
Alona <alena.tsapenko7@gmail.com>
2024-08-15 09:58:35 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -39,7 +39,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: List) -> int:
-    total_food = 0
-    for animal in animals:
-        total_food += animal.feed()
-    return total_food
+    return sum(animal.feed() for animal in animals)
