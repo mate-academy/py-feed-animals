651a3ddc85bfebafeecd999a27a0fedb632822b6
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-08-29 20:29:05 +0300

    Fix Spaces

--- app/main.py
+++ app/main.py
@@ -1,46 +1,46 @@
 class Animal:
-	def __init__(
-		self,
-		name: str,
-		appetite: int = 0,
-		is_hungry: bool = True
-	) -> None:
-		self.name = name
-		self.appetite = appetite
-		self.is_hungry = is_hungry
+    def __init__(
+        self,
+        name: str,
+        appetite: int = 0,
+        is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
 
-	def print_name(self) -> None:
-		print(f"Hello, I'm {self.name}")
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
 
-	def feed(self) -> int:
-		if self.is_hungry:
-			print(f"Eating {self.appetite} food points...")
-			self.is_hungry = False
-			return self.appetite
-		return 0
+    def feed(self) -> int:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        return 0
 
 
 class Cat(Animal):
-	def __init__(self, name: str, is_hungry: bool = True) -> None:
-		super().__init__(name=name, appetite=3, is_hungry=is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name=name, appetite=3, is_hungry=is_hungry)
 
-	@staticmethod
-	def catch_mouse() -> None:
-		print("The hunt began!")
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
 
 
 class Dog(Animal):
-	def __init__(self, name: str, is_hungry: bool = True) -> None:
-		super().__init__(name=name, appetite=7, is_hungry=is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name=name, appetite=7, is_hungry=is_hungry)
 
-	@staticmethod
-	def bring_slippers() -> None:
-		print("The slippers delivered!")
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
 
 
 def feed_animals(animals: list[Animal]) -> int:
-	result = 0
-	for animal in animals:
-		if animal.is_hungry:
-			result += animal.feed()
-	return result
+    result = 0
+    for animal in animals:
+        if animal.is_hungry:
+            result += animal.feed()
+    return result
