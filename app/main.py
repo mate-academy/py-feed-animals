2f790ad370dcdddc06a9d21d5defee7d341684da
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-09-09 17:04:10 +0300

    changed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Animal:
     def __init__(
         self,
@@ -43,5 +46,5 @@ class Dog(Animal):
         print("The slippers delivered!")
 
 
-def feed_animals(animals: list) -> int:
+def feed_animals(animals: List[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
