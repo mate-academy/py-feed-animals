45c5844040ad71a9a75c534e32f4789126631880
Nazar <nazargorodyko@gmail.com>
2024-09-21 01:18:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,55 @@
-#  write your code here
+from __future__ import annotations
+
+
+class Animal:
+    animal_list = []
+
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+        Animal.animal_list.append(self)
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = not self.is_hungry
+
+            return self.appetite
+
+        else:
+            return 0
+
+
+class Cat(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
+
+    @staticmethod
+    def catch_mouse() -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(other: list[Animal]) -> int:
+    summary = 0
+    for i in range(len(other)):
+        if isinstance(other[i], Animal):
+            summary += Animal.feed(other[i])
+    return summary
