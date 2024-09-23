95e4f3d3f78131fc438bc2ae5cacae5f4dc00dd0
dmytro <dmysamo@gmail.com>
2024-09-23 13:37:15 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 class Animal:
 
-    def __init__(self, name: str,
+    def __init__(self,
+                 name: str,
                  appetite: int,
                  is_hungry: bool = True) -> None:
         self.name = name
@@ -20,7 +21,8 @@ class Animal:
 
 class Cat(Animal):
 
-    def __init__(self, name: str,
+    def __init__(self,
+                 name: str,
                  is_hungry: bool = True) -> None:
         super().__init__(name=name, appetite=3,
                          is_hungry=is_hungry)
@@ -32,7 +34,8 @@ class Cat(Animal):
 
 class Dog(Animal):
 
-    def __init__(self, name: str,
+    def __init__(self,
+                 name: str,
                  is_hungry: bool = True) -> None:
         super().__init__(name=name, appetite=7,
                          is_hungry=is_hungry)
