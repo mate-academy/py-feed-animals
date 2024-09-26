62ad89727a22e8bc5ad4a98006055ad422d5c2ce
Max <mxbely98@gmail.com>
2024-09-26 13:48:44 +0300

    simplification resolution

--- app/main.py
+++ app/main.py
@@ -18,8 +18,7 @@ class Animal:
             self.is_hungry = False
             print(f"Eating {self.appetite} food points...")
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
