4fd4028b8e2e00ab590bc35bf01e1b7da70ca5cf
Bohdan Poliienko <mignatov756@gmail.com>
2024-10-03 00:15:59 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,64 @@
-#  write your code here
+class Animal:
+    animals = []
+
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+        Animal.animals.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{name: {self.name}, "
+            f"appetite: {self.appetite}, "
+            f"is_hungry: {self.is_hungry}}}"
+        )
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if not self.is_hungry or self.appetite == 0:
+            return 0
+        else:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+
+
+class Cat(Animal):
+    def __init__(
+            self,
+            name: str,
+            appetite: int = 3,
+            is_hungry: bool = True
+    ) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    def catch_mouse(self) -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(
+            self, name: str,
+            appetite: int = 7,
+            is_hungry: bool = True
+    ) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    def bring_slippers(self) -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(animals: list[Animal]) -> int:
+    sum_of_food_points = 0
+    for animal in animals:
+        sum_of_food_points += animal.feed()
+
+    return sum_of_food_points
