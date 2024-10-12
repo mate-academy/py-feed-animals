b061a7286ad9bd16b8bf02aba4e4d550027a6ffb
Ihor Chaikovskyy <Altairerko@gmail.com>
2024-10-12 21:15:09 +0300

    Feed Animals Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 class Animal:
-    alive = []
+    animals_alive = []
 
     def __init__(self, name: "str", appetite: int,
                  is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
-        Animal.alive.append(self)
+        Animal.animals_alive.append(self)
 
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
@@ -16,8 +16,7 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
@@ -37,7 +36,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    total_food_points = 0
-    for animal in animals:
-        total_food_points += animal.feed()
-    return total_food_points
+    return sum(animal.feed() for animal in animals)
