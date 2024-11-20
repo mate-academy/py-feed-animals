965bcea1e7fcbc05a502d96726e557715605ce00
Insarov Kuzma <kuzma.insarov813@gmail.com>
2024-11-20 08:27:13 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from turtle import Turtle
+
+
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry: bool = 1) -> None:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -10,13 +13,13 @@ class Animal:
     def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
-            self.is_hungry = 0
+            self.is_hungry = False
             return self.appetite
         return 0
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = 1) -> None:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 3, is_hungry)
 
     def catch_mouse(self) -> None:
@@ -24,7 +27,7 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = 1) -> None:
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 7, is_hungry)
 
     def bring_slippers(self) -> None:
