1d8640656f253b406d7a1fceae0a24e5238655bc
Mazurkav <aleshkamazurkevich@gmail.com>
2024-11-20 17:51:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -14,27 +14,25 @@ class Animal:
             return self.appetite
         return 0
 
+
 class Cat(Animal):
     def __init__(self, name, is_hungry=True):
-        super().__init__(name, 3, is_hungry)
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self):
         print("The hunt began!")
-        # Let's set the cat to be hungry after catching a mouse, implying it gets an appetite.
-        self.is_hungry = True
+
 
 class Dog(Animal):
     def __init__(self, name, is_hungry=True):
-        super().__init__(name, 7, is_hungry)
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     def bring_slippers(self):
         print("The slippers delivered!")
-        # Let's make the dog less hungry after delivering slippers, simulating the reward.
-        if self.is_hungry:
-            self.is_hungry = False  # Dog isn't hungry anymore after delivering slippers.
+
 
 def feed_animals(animals):
-    total_food = 0
+    total_food_points = 0
     for animal in animals:
-        total_food += animal.feed()
-    return total_food
+        total_food_points += animal.feed()
+    return total_food_points
