acf89538fb58ee7e774198e771d2319346a87a4b
Julia Alexeeva <julia4406@gmail.com>
2024-12-03 14:23:18 +0200

    Changed feed_animals func, fixed style defects.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 class Animal:
-    def __init__(self,
-                 name: str,
-                 appetite: int,
-                 is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            appetite: int,
+            is_hungry: bool = True
+    ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -19,9 +21,11 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self,
-                 name: str,
-                 is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True
+    ) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
@@ -30,9 +34,11 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self,
-                 name: str,
-                 is_hungry: bool = True) -> None:
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True
+    ) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     @staticmethod
@@ -41,4 +47,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    return sum([beast.feed() for beast in animals])
+    return sum(beast.feed() for beast in animals)
