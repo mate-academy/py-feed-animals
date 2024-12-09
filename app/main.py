31c67d71a58cd7ed5a2ab2667fd8075b50f31071
AntonKrinichnyi <a.krinichnyi@gmail.com>
2024-12-09 22:18:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class Animal:
         self.is_hungry = is_hungry
 
     def print_name(self) -> None:
-        print(f"Hello, i'm {self.name}")
+        print(f"Hello, i\'m {self.name}")
 
     def feed(self) -> int:
         if self.is_hungry is True:
