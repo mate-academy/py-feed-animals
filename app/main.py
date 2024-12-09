3a0d5d11addb8cfaaf122ebcfbe9deb9ef474e17
Arsenii Aristov <arseniyaristov07@gmail.com>
2024-12-09 17:30:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ class Cat(Animal):
 class Dog(Animal):
     def __init__(self, name : str,
                  is_hungry : bool = True) -> None:
-        super().__init__(name,appetite = 7,is_hungry = True)
+        super().__init__(name, appetite=7, is_hungry=True)
 
     @staticmethod
     def bring_slippers() -> None:
