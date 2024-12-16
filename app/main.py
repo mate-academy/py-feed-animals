333e3cbb80537e5e22764dce2095e5e8f2088bda
Olha Yukhnenko <pyolh36@gmail.com>
2024-12-16 20:57:08 +0200

    fix_flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry=True) -> None:
+    def __init__(self, name: str, appetite: int, is_hungry: bool=True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -17,7 +17,7 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry=True) -> None:
+    def __init__(self, name: str, is_hungry: bool=True) -> None:
         super().__init__(name, 3, is_hungry)
 
     @staticmethod
@@ -26,11 +26,11 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry=True) -> None:
+    def __init__(self, name: str, is_hungry: bool=True) -> None:
         super().__init__(name, 7, is_hungry)
 
     @staticmethod
-    def bring_slippers():
+    def bring_slippers() -> None:
         print("The slippers delivered!")
 
 
