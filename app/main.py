d1de33c691bfd06b80035e0530baebaeb3d32436
Miwacer <il>
2024-12-24 01:00:10 +0200

    Solution 2.1

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ class Animal():
         self.appetite = appetite
         self.is_hungry = is_hungry
 
+
     def print_name(self):
         print(f"Hello, I'm {self.name}")
+
 
     def feed(self):
         if self.is_hungry:
@@ -15,9 +17,11 @@ class Animal():
         else:
             return 0
 
+
 class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True):
         super().__init__(name, appetite=3, is_hungry=is_hungry)
+
 
     def catch_mouse(self):
         print("The hunt began!")
@@ -27,8 +31,10 @@ class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True):
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
+
     def bring_slippers(self):
         print("The slippers delivered!")
+
 
 def feed_animals(animals: list) -> int:
     return sum(animal.feed() for animal in animals)
