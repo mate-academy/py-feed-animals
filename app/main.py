2e4e37245142c348b879e63968acf3746491d9ea
Svitlana <svetapruta@gmail.com>
2025-01-09 14:21:06 +0100

    create class Animal

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-#  write your code here
+class Animal:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.is_hungry is True:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            Animal.food_points = self.appetite
+            return Animal.food_points
+        return 0
