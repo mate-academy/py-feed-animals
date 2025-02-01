75aaa94872b353039baec4afc9d4d40fa204f36c
Marek Smolana <msmolana@gmail.com>
2025-02-01 20:31:34 +0100

    corrected typo

--- app/main.py
+++ app/main.py
@@ -4,17 +4,17 @@ from __future__ import annotations
 class Animal:
     def __init__(self, *args, **kwargs) -> None:
         self.is_hungry = True
-        self.apetite = 0
+        self.appetite = 0
         if len(args) > 0:
             self.name = args[0]
         if len(args) > 1:
-            self.apetite = args[1]
+            self.appetite = args[1]
         if len(args) > 2: 
             self.is_hungry = args[2]
         if "name" in kwargs:
             self.name = kwargs["name"]
-        if "apetite" in kwargs:
-            self.apetite = kwargs["apetite"]
+        if "appetite" in kwargs:
+            self.appetite = kwargs["appetite"]
         if "is_hungry" in kwargs:
             self.is_hungry = kwargs["is_hungry"]
 
@@ -22,12 +22,12 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
-        apetite = 0
+        appetite = 0
         if self.is_hungry:
-            apetite = self.apetite
+            appetite = self.appetite
             self.is_hungry = False
-            print(f"Eating {self.apetite} food points...")
-        return apetite
+            print(f"Eating {self.appetite} food points...")
+        return appetite
 
 
 class Cat(Animal):
