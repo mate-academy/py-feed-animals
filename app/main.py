6ffacbb9bbcc6b3175c31a4314a5ba0ca9ac545d
Roman Kostiv <rkostiv@gmail.com>
2025-02-07 23:33:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,51 @@
-#  write your code here
+class Animal:
+
+    def __init__(self, name: str,
+                 appetite: int,
+                 is_hungry: bool = True) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def __repr__(self) -> str:
+        return (f"{{{self.name}, {self.appetite}, {self.is_hungry}}}")
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> None:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = not self.is_hungry
+            return self.appetite
+        if not self.is_hungry:
+            return 0
+
+    @staticmethod
+    def feed_animals(*animals) -> int:
+        total_food = 0
+        for animal in animals:
+            total_food += animal.feed()
+        return total_food
+
+
+class Cat(Animal):
+
+    def __init__(self, name: str,
+                 is_hungry: bool = True,
+                 appetite: int = 3) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    def catch_mouse(self) -> None:
+        print("The hunt began!")
+
+
+class Dog(Animal):
+
+    def __init__(self, name: str,
+                 is_hungry: bool = True,
+                 appetite: int = 7) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    def bring_slippers(self) -> None:
+        print("The slippers delivered!")
